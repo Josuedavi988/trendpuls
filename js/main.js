243abdc64096143179e4b0ed542a00686387b9a9
Josuedavi988 <150014117+Josuedavi988@users.noreply.github.com>
2026-01-25 23:53:29 +0100

    .

--- js/main.js
+++ js/main.js
@@ -18,36 +18,6 @@ document.querySelectorAll(".video-wrapper").forEach(wrapper => {
 });
 
 
-const dot = document.getElementById('cursor-dot');
-  const ring = document.getElementById('cursor-ring');
-
-  let mouseX = 0;
-  let mouseY = 0;
-  let ringX = 0;
-  let ringY = 0;
-
-  document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-    dot.style.left = mouseX + 'px';
-    dot.style.top = mouseY + 'px';
-  });
-
-  function animate() {
-    ringX += (mouseX - ringX) * 0.15;
-    ringY += (mouseY - ringY) * 0.15;
-
-    ring.style.left = ringX + 'px';
-    ring.style.top = ringY + 'px';
-
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-
-
-  
 document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
   link.addEventListener('click', () => {
     const navbarCollapse = document.querySelector('.navbar-collapse');
